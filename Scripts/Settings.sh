53fc9e2dea4b6bf23599b663a3ffdd45e750dbf4
Star_ZER0 <star_zer0@163.com>
2023-04-05 13:17:44 +0800

    fix default theme

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -7,6 +7,6 @@ sed -i "s/OpenWrt/$LEDE_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认IP地址
 sed -i "s/192.168.1.1/$LEDE_IP/g" ./package/base-files/files/bin/config_generate
 #修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/lean/autocore/files/ -type f -name "index.htm")

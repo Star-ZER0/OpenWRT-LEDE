7955484c7f7dfc4b5d29c9e07af606aebf9b1883
Star_ZER0 <star_zer0@163.com>
2023-04-05 14:30:46 +0800

    Update Settings-ImmortalWrt.sh

--- Scripts/Settings-ImmortalWrt.sh
+++ Scripts/Settings-ImmortalWrt.sh
@@ -9,4 +9,4 @@ sed -i "s/192.168.1.1/$IMMORTALWRT_IP/g" ./package/base-files/files/bin/config_g
 #修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 #修改默认时间格式
-sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/lean/autocore/files/ -type f -name "index.htm")
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/immortalwrt/autocore/files/ -type f -name "index.htm")

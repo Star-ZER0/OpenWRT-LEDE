622c62d1baebdd6ff5408662b8163844703abe92
Star_ZER0 <star_zer0@163.com>
2023-04-05 17:40:57 +0800

    Update Settings-ImmortalWrt.sh

--- Scripts/Settings-ImmortalWrt.sh
+++ Scripts/Settings-ImmortalWrt.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#删除冲突主题
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\).*")
 #修改默认主机名
 sed -i "s/OpenWrt/$IMMORTALWRT_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认IP地址
 sed -i "s/192.168.1.1/$IMMORTALWRT_IP/g" ./package/base-files/files/bin/config_generate
-#修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile

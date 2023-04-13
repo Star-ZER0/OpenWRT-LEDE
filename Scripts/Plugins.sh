a379c6472d52f97125b3e4d547932461694ae5e5
Star_ZER0 <star_zer0@163.com>
2023-04-13 22:49:47 +0800

    Create Plugins.sh

---
+++ Scripts/Plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Design Theme
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-theme-design.git
+#Design Config
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git

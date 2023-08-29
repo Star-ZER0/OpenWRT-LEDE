ef3b18e77a93db7d0e7e02787bce9c9dc4bca446
Star_ZER0 <star_zer0@163.com>
2023-08-29 14:40:05 +0800

    Update Settings-LEDE.sh

--- Scripts/Settings-LEDE.sh
+++ Scripts/Settings-LEDE.sh
@@ -4,3 +4,5 @@
 sed -i "s/OpenWrt/$LEDE_NAME/g" ./package/base-files/files/bin/config_generate
 #修改默认IP地址
 sed -i "s/192.168.1.1/$LEDE_IP/g" ./package/base-files/files/bin/config_generate
+#修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile

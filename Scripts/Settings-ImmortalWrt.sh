0a717262499699dee60268af5e3ef44f685fda41
Star_ZER0 <star_zer0@163.com>
2023-08-29 14:39:50 +0800

    Update Settings-ImmortalWrt.sh

--- Scripts/Settings-ImmortalWrt.sh
+++ Scripts/Settings-ImmortalWrt.sh
@@ -5,4 +5,4 @@ sed -i "s/ImmortalWrt/$IMMORTALWRT_NAME/g" ./package/base-files/files/bin/config
 #修改默认IP地址
 sed -i "s/192.168.1.1/$IMMORTALWRT_IP/g" ./package/base-files/files/bin/config_generate
 #修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
